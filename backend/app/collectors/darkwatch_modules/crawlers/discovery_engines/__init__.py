4708bef469d029922481ffc0121496e1223b0ecc
cipher-x-sudo <cipher-x-sudo@users.noreply.github.com>
2025-12-13 06:35:39 +0500

    Remove Reddit search functionality completely

--- backend/app/collectors/darkwatch_modules/crawlers/discovery_engines/__init__.py
+++ backend/app/collectors/darkwatch_modules/crawlers/discovery_engines/__init__.py
@@ -5,7 +5,6 @@ Provides URL discovery from various sources.
 """
 
 from .gist_engine import GistEngine
-from .reddit_engine import RedditEngine
 from .security_news_engine import SecurityNewsEngine
 from .onionsearch import DarkWebEngine  # DarkWebEngine is now in onionsearch.py
 from .search_engine import SearchEngine
@@ -13,7 +12,6 @@ from .pastebin_engine import PastebinEngine
 
 __all__ = [
     'GistEngine',
-    'RedditEngine',
     'SecurityNewsEngine',
     'DarkWebEngine',
     'SearchEngine',
