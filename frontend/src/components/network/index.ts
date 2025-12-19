f6f23df481e07cbdfb1e9c312ee7c7c0de948c3d
cipher-x-sudo <cipher-x-sudo@users.noreply.github.com>
2025-12-20 03:02:34 +0500

    Fix: Rename __init__.ts to index.ts for Next.js module resolution

---
+++ frontend/src/components/network/index.ts
@@ -0,0 +1,6 @@
+export { LiveStream } from "./LiveStream";
+export { FilterPanel } from "./FilterPanel";
+export { StatisticsPanel } from "./StatisticsPanel";
+export { TunnelAlertsPanel } from "./TunnelAlertsPanel";
+export { BlockManager } from "./BlockManager";
+
