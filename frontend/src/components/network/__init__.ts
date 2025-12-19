eb9d50ded1b9804fe3212fe7fff285280ba2589c
cipher-x-sudo <cipher-x-sudo@users.noreply.github.com>
2025-12-20 03:02:45 +0500

    Remove old __init__.ts file

--- frontend/src/components/network/__init__.ts
+++
@@ -1,6 +0,0 @@
-export { LiveStream } from "./LiveStream";
-export { FilterPanel } from "./FilterPanel";
-export { StatisticsPanel } from "./StatisticsPanel";
-export { TunnelAlertsPanel } from "./TunnelAlertsPanel";
-export { BlockManager } from "./BlockManager";
-
